64a20208df723301f79aa3c9dfa147d0062be652
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2024-01-24 00:39:24 +0530

    Added Service Registry(eureka server), load balancing

--- employee-service/src/main/java/net/floppy/employeeservice/service/Impl/APIFeignClient.java
+++ employee-service/src/main/java/net/floppy/employeeservice/service/Impl/APIFeignClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080/", value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080/", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE") //here, We are provided Service ID which is in Eureka UI, So make load balancing
 public interface APIFeignClient {
     @GetMapping("api/departments/{department_code}")
     DepartmentDto GetDepartmentCode(@PathVariable("department_code") String departmentCode);
