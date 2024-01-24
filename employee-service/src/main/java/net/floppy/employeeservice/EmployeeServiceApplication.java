7779bcc2cc36f8b82d4c41534706d9dd7539696a
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2024-01-24 23:16:05 +0530

    Added Service Registry(eureka server), load balancing

--- employee-service/src/main/java/net/floppy/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/floppy/employeeservice/EmployeeServiceApplication.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +21,7 @@ public class EmployeeServiceApplication {
 	}
 
 //	@Bean
+//	@LoadBalanced
 //	public RestTemplate restTemplate()
 //	{
 //		return new RestTemplate();
