56b477b751bacd95ccf8b95bf65ba59d79813c11
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2024-01-26 22:54:00 +0530

    Api Gateway  and automatic Routing added

---
+++ api-gateway/src/main/java/net/floppy/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package net.floppy.apigateway;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
