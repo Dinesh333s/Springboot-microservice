eaaf205eff7a0f0e5b721985300cf1e7f000ab81
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2024-01-28 20:51:40 +0530

    FrontEnd Added

--- api-gateway/src/main/java/net/floppy/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/net/floppy/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,7 @@ import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
